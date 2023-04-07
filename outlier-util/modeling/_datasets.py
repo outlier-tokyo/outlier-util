12ef1b8fcba155d07d57d89c1af6f084ba4d3a8e
knishimura <keisuke.nishimura@outlier.tokyo>
2023-04-07 16:47:15 +0900

    add #1 create folder

---
+++ outlier-util/modeling/_datasets.py
@@ -0,0 +1,26 @@
+"""
+モデリング前のデータセット整理に関する便利機能
+"""
+
+def _template():
+    """
+    <概要>
+    Parameters
+    ----------
+    <param_name> : <format>, <default>
+        <describe>
+    Returns
+    ----------
+    <name> : <format>, <default>
+        <describe>
+    See Also
+    --------
+    <xxx>
+    Notes
+    -----
+    <xxx>
+    Examples
+    --------
+    <xxx>
+    """
+    pass
